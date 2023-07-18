fdefb3e1f65feb3fee5fff3acd5624247dee8b74
Jumpei-Arigaya <junpeeei0407@gmail.com>
2023-07-18 23:52:29 +0900

    カスタム関数

--- php_test/index.php
+++ php_test/index.php
@@ -287,3 +287,45 @@ switch ($data) {
         echo 1;
         break;
 }
+
+echo '<br>';
+
+// カスタム関数
+
+function test()
+{
+    echo 'test';
+}
+
+test();
+
+echo '<br>';
+
+$comment = 'コメント2';
+function getComment($string)
+{
+    echo $string;
+}
+
+getComment($comment);
+
+echo '<br>';
+
+function getNumberOfComment()
+{
+    return 5;
+}
+
+$commentNumber = getNumberOfComment();
+
+var_dump($commentNumber);
+
+echo '<br>';
+
+function sumPrice($int1, $int2)
+{
+    $int3 = $int1 + $int2;
+    return $int3;
+}
+
+var_dump(sumPrice(4, 3));
